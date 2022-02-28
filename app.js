33b469cd65cacc169cdadcd643866d4b283010e2
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-02-28 18:46:23 +0500

    add models

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/not-found-error');
+require('dotenv').config();
+
+const {
+  createUser, login,
+} = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 
+const ERROR_DEFAULT = 500;
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.post('/signup', createUser);
+app.post('/signin', login);
+
+app.use(auth);
+
+app.use('/users', require('./routes/users'));
+
+app.use('/movies', require('./routes/movies'));
+
+// app.use(errorLogger);
+
+app.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+
+// app.use(errors());
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || ERROR_DEFAULT;
+  const message = statusCode === ERROR_DEFAULT ? 'На сервере произошла ошибка' : err.message;
+  res.status(statusCode).send({ message });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const NoAuthError = require('../errors/no-auth-error');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new NoAuthError('Необходима авторизация');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret');
+  } catch (error) {
+    // throw new NoAuthError('Необходима авторизация');
+    console.log(process.env.JWT_SECRET);
+  }
+
+  req.user = payload;
+  next();
+
+  return false;
+};
