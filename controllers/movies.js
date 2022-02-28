30bedbea955dbd0a597483b61c5941a4b4ee3428
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-02-28 18:44:55 +0500

    add controllers

---
+++ controllers/movies.js
@@ -0,0 +1,41 @@
+const Movie = require('../models/movie');
+
+const NotFoundError = require('../errors/not-found-error');
+const NotCorrectError = require('../errors/not-correct-error');
+const NoRightsError = require('../errors/no-rights-error');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({ owner: req.user._id })
+    .then((movies) => res.send({ data: movies }))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const { country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId } = req.body;
+
+  Movie.create({ country, director, duration, year, description, image, trailer, nameRU, nameEN, thumbnail, movieId, owner: req.user._id })
+    .then((movie) => res.send({ data: movie }))
+    .catch((error) => {
+      if (error.name === 'ValidationError') { next(new NotCorrectError('Некорректные данные')); } else {
+        next(error);
+      }
+    });
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.movieId)
+    .orFail(new Error('Not Found'))
+    .then((card) => {
+      if (card.owner.toString() !== req.user._id.toString()) {
+        throw new Error('No rights');
+      }
+
+      return card.remove();
+    })
+    .then(() => res.send({ message: 'Фильм успешно удален' }))
+    .catch((error) => {
+      if (error.name === 'CastError') { next(new NotCorrectError('Некорректный id')); } else if (error.message === 'Not Found') { next(new NotFoundError('Запрашиваемый фильм не найден')); } else if (error.message === 'No rights') { next(new NoRightsError('Нет прав на удаление фильма')); } else {
+        next(error);
+      }
+    });
+};
---
+++ controllers/users.js
@@ -0,0 +1,68 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
+
+const NotFoundError = require('../errors/not-found-error');
+const NotCorrectError = require('../errors/not-correct-error');
+const NoAuthError = require('../errors/no-auth-error');
+const ExistEmailError = require('../errors/exist-email-error');
+
+module.exports.login = (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  return User.findUserByCredentials(email, password, name)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+
+      res.send({ token });
+    })
+    .catch((error) => {
+      if (error.name === 'Неправильные почта, имя или пароль') { next(new NoAuthError('Неправильные почта, имя или пароль')); } else {
+        next(error);
+      }
+    });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, about, avatar, email,
+  } = req.body;
+
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res.send({
+      data: {
+        name: user.name, about: user.about, avatar: user.avatar, email: user.email,
+      },
+    }))
+    .catch((error) => {
+      if (error.name === 'ValidationError') { next(new NotCorrectError('Некорректные данные')); } else if (error.name === 'MongoServerError' && error.code === 11000) { next(new ExistEmailError('Пользователь с таким email уже существует')); } else {
+        next(error);
+      }
+    });
+};
+
+module.exports.getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail(new Error('Not Found'))
+    .then((user) => res.send({ data: user }))
+    .catch((error) => {
+      if (error.message === 'Not Found') { next(new NotFoundError('Запрашиваемый пользователь не найден')); } else {
+        next(error);
+      }
+    });
+};
+
+module.exports.updateUser = (req, res, next) => {
+  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+    .orFail(new Error('Not Found'))
+    .then((user) => res.send({ data: user }))
+    .catch((error) => {
+      if (error.name === 'ValidationError') { next(new NotCorrectError('Некорректные данные')); } else if (error.message === 'Not Found') { next(new NotFoundError('Запрашиваемый пользователь не найден')); } else {
+        next(error);
+      }
+    });
+};
